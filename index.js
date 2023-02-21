86f0a223d30275d2c7e76022838810c324965b8e
maniR1990 <mani.r16@gmail.com>
2023-02-21 07:56:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function first() {
+  return Promise.resolve(1);
+}
+
+function second(value) {
+  return Promise.resolve(value + 1);
+}
+
+first()
+  .then((value) => second(value))
+  .then((valueTwo) => console.print(valueTwo));
