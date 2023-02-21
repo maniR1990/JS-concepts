9d464cac76241741396159e1277467f56953e2ce
maniR1990 <mani.r16@gmail.com>
2023-02-21 16:07:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -31,24 +31,100 @@ function sceneTwofnTwo(value) {
 }
 
 sceneTwofnOne()
+
   // then(cbfn)
   // then(wrappedPromise(cbfn))
 
   .then((val) => sceneTwofnTwo(val))
   .then((val) => console.log('scenario 2:', val));
 
+// scenario3 : throwing an error outside the promise chain wont go to catchBlock.
 
+/**
+ * when the error occured before the promise chain starts. then this error will be considered as run-time error.  it wont go promise catch block.
+ * reason: error occured before executing the promise
+ *
+ */
+function sceneThreefnOne() {
+  throw new Error('okay im an error');
+  return Promise.resolve(1);
+}
 
-  // scenario3 : throwing an error outside the promise chain wont go to catchBlock.
+sceneThreefnOne()
+  .then((value) => console.log(value))
+  .catch((err) => {
+    console.log('inside catch Block');
+    console.log('error caught--', err.message);
+  });
 
-  function sceneThreefnOne() {
-    throw new Error("okay im an error")
-    return Promise.reject(1);
+// scenario4 : throwing an error inside the promise chain
+/**
+ * Here error is thrown inside the promise chain block.
+ * In function "sceneFourfnOne" return promise chain obj.
+ * while resolving it with thenable fn, then "sceneFourfnOne" is called.
+ * Inside the "sceneFourfnOne" a error is thrown, which is run time error.
+ * this error happens inside the promise chain, so this error is bubbled up to nearest catch block
+ *
+ */
+
+function sceneFourfnOne() {
+  return Promise.resolve(1);
+}
+
+function sceneFourfnTwo(val) {
+  throw new Error('okay im an error');
+  return Promise.resolve(val);
+}
+
+sceneFourfnOne()
+  .then((val) => sceneFourfnTwo(val))
+  .then((val) => console.log(val))
+  .catch((err) => {
+    console.log('inside catch Block');
+    console.log('error caught--', err.message);
+  });
+
+// scenario five: Promise with try catch
+function sceneFivefnone(val) {
+  try {
+    return sceneFivefnTwo();
+  } catch (err) {
+    console.log('try/catch', err);
   }
-  
-  sceneThreefnOne()
-  .then(value=>console.log(value))
-  .catch((err)=>{
-    console.log("inside catch Block");
-    console.log("error caught--", err.message)
-  })
+}
+
+function sceneFivefnTwo() {
+  return Promise.reject('something went wrong!');
+}
+
+sceneFivefnone()
+  .then(() => console.log('first'))
+  .catch((err) => {
+    console.log('main catch', err);
+  });
+
+// scenario six: resolve is not return statement
+
+
+function sceneSixfnOne() {
+  sceneSixfnTwo()
+  .then((val)=>console.log(val)) // 1 
+  .catch((err)=>console.log(err));
+}
+
+function sceneSixfnTwo() {
+  return new Promise((resolve, reject) => {
+    resolve(1);
+    reject('failed');
+    resolve(2);
+    console.log('after resolve/reject');
+  });
+}
+
+sceneSixfnOne()
+
+/*
+output 
+'after resolve/reject'
+1
+**/
